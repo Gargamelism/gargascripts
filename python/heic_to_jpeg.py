a8d0e93861220a0fde359543d821e446226554b1
Gargamel Harasha <rohi.ulecia@gmail.com>
2024-10-14 13:02:48 +0300

    heic to jpeg folder traverser

---
+++ python/heic_to_jpeg.py
@@ -0,0 +1,40 @@
+import argparse
+import os
+from PIL import Image
+from pillow_heif import register_heif_opener
+
+
+def traverse_dir(base_path, cb):
+    for root, dirs, files in os.walk(base_path, topdown=True):
+        for dir in dirs:
+            traverse_dir(os.path.join(root, dir), cb)
+
+        for file in files:
+            cb(os.path.join(root, file))
+
+
+def heic_to_jpg(file_path):
+    if file_path.endswith(".heic"):
+        print(f"converting {file_path} to jpg")
+        my_pic = Image.open(file_path)  # opening .heic images
+        jpg_pic_name = (
+            file_path.split(".")[0] + ".jpg"
+        )  # creating new names for .jpg images
+        my_pic.save(jpg_pic_name, format="JPEG", optimize=True, quality=100)
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        description="calculate duration times in given file"
+    )
+    parser.add_argument("base_path")
+    args = parser.parse_args()
+
+    register_heif_opener()
+    traverse_dir(args.base_path, heic_to_jpg)
+
+    print("done!")
+
+
+if __name__ == "__main__":
+    main()
